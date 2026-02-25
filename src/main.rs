e3702cebb00e69ee4d8d8ec519f83096a8d52184
LutiDrist <gdm1304@mail.ru>
2026-02-25 22:27:19 +0300

    start making handshake server client

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use std::io::{BufReader, Read, Write};
 
 
-
-
+use rand::Rng;
 fn handle_client(mut stream: TcpStream) {
     // SSH всегда начинает с обмена версиями в виде строки
     // Пример: "SSH-2.0-OpenSSH_9.0\r\n"
@@ -43,6 +42,64 @@ fn handle_client(mut stream: TcpStream) {
 
     println!("Valid SSH client detected. Proceeding to next stage...");
 }
+
+fn build_packet(payload: Vec<u8>) -> Vec<u8> { //функция создания пакета, скорее всего идет речь о пакете, который будет проивзодить связь сервер клиент
+    let block_size =8; // инициализируем первую переменную для предоставления обьема данных по обмену
+
+    let mut padding_lenght = block_size - ((payload.len() + 5) % block_size);
+    if padding_lenght < 4 {
+        padding_lenght += block_size;
+    }
+// все что вверху абсолютно не понятно
+
+    let packet_lenght = payload.len() + padding_lenght + 1; // тоже не понимаю для чего у нас есть длинна пакета
+
+    let mut packet = Vec::new(); // инициализируем сам пакет
+
+    packet.extend_from_slice(&(packet_lenght as u32).to_be_bytes()); // extend_from_slice меня пугает я не понимаю что оно делает 
+    packet.push(padding_lenght as u8);
+    packet.extend_from_slice(&payload);
+    packet.extend(vec![0u8; padding_lenght]);
+
+
+    packet // эта функция скорее всего по мои каким то наблюдениям пушит первый пакет для соединения, то есть открытый ключ после обмена которым соединение появится
+}
+
+fn write_namelist(list: &str, buffer: &mut Vec<u8>) {
+    buffer.extend_from_slice(&(list.len() as u32).to_be_bytes());
+    buffer.extend_from_slice(list.as_bytes());
+} //абсолютно не понимаю что это 
+
+fn send_kexinit(stream: &mut TcpStream) -> std::io::Result<()> { //kexinit нихуя не понятная штука.. Что это вообще тут написано?
+    let mut payload = Vec::new();
+
+    payload.push(20);
+
+    let cookie = [0u8; 16];// этого нет в пакете rand 
+    payload.extend_from_slice(&cookie);//  эта строка и все что ниже нее один большой тупик
+
+    write_namelist("deffie-hellman-group-sha254", &mut payload);
+    write_namelist("ssh-rsa", &mut payload);
+    write_namelist("aes128-ctr", &mut payload);
+    write_namelist("aes128-ctr", &mut payload);
+    write_namelist("hmac-sha2-256", &mut payload);
+    write_namelist("hmac-sha2-256", &mut payload);
+    write_namelist("none", &mut payload);
+    write_namelist("none", &mut payload);
+    write_namelist("", &mut payload);
+    write_namelist("", &mut payload);
+
+    payload.push(0);
+    payload.extend_from_slice(&0u32.to_be_bytes());
+
+    let packet = build_packet(payload);
+    stream.write_all(&packet)?;
+
+    println!("Sent SSH_MSG_KEXINIT");
+
+    Ok(())
+}
+
 
 fn main() {
     // Слушаем TCP порт (пока любой, не 22)
